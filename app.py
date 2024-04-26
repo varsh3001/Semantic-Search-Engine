6bf1247b7847a71286fdc6033712f99db27efd66
varshini_01 <139046539+varsh3001@users.noreply.github.com>
2024-04-26 22:54:30 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,33 @@
+import streamlit as st
+import pandas as pd
+import chromadb
+from chromadb.utils import embedding_functions
+
+CHROMA_DATA_PATH="chroma_data/"
+EMBED_MODEL="all-MiniLM-L6-v2"
+COLLECTION_NAME="subtitles"
+
+client=chromadb.PersistentClient(path=CHROMA_DATA_PATH)
+
+embedding_func = embedding_functions.SentenceTransformerEmbeddingFunction(model_name=EMBED_MODEL)
+c = client.get_collection(name=COLLECTION_NAME, embedding_function=embedding_func)
+
+
+
+st.title("⌨️Subtitle Search Bar")
+bg="""
+<style>
+[data-testid="stAppViewContainer"]{
+  background-color:	#8C90C8;
+}
+</style>
+"""
+st.markdown(bg,unsafe_allow_html=True)
+
+label = "Enter the subtitle"
+query = st.text_input(label)
+if st.button("Find the movie/series name"):
+    query_results = c.query(query_texts=[query], n_results=5)
+    st.write("Top 5 similar")
+    st.write(query_results["metadatas"])
+  
